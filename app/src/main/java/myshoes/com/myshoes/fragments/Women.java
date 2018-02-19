7c50ee0027cfe49ae0c2821d760fe54f54e57ab2
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-02-19 11:11:34 +0300

    Initial Commit

--- app/src/main/java/myshoes/com/myshoes/fragments/Women.java
+++ app/src/main/java/myshoes/com/myshoes/fragments/Women.java
@@ -1,10 +1,27 @@
 package myshoes.com.myshoes.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import myshoes.com.myshoes.R;
 
 /**
  * Created by gopinath on 12/02/18.
  */
 
 public class Women extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View v = inflater.inflate(R.layout.women_category, container, false);
+
+        return v;
+    }
 }
