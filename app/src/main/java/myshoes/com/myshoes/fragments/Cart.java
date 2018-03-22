c4483fe23619084846e7ff3d9f9a4bcbff1ed207
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2018-03-22 15:27:16 +0300

    Updated Commit!

--- app/src/main/java/myshoes/com/myshoes/fragments/Cart.java
+++ app/src/main/java/myshoes/com/myshoes/fragments/Cart.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,14 +27,14 @@ public class Cart extends Fragment implements StoreAdapter.ListItemClickListener
     RecyclerView recyclerView;
     CartAdapter cAdapter;
     ArrayList<myshoes.com.myshoes.model.Cart> mCartProducts;
-    TextView total;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.cart_recycler, container, false);
         recyclerView = v.findViewById(R.id.cart_recycler_view);
-        total = v.findViewById(R.id.cart_total_price);
+//        total = v.findViewById(R.id.cart_total_price);
 
         mCartProducts = new ArrayList<myshoes.com.myshoes.model.Cart>();
         mCartProducts = (ArrayList<myshoes.com.myshoes.model.Cart>) retrieveFromDatabase();
@@ -44,20 +43,19 @@ public class Cart extends Fragment implements StoreAdapter.ListItemClickListener
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(cAdapter);
-        total.setText(String.valueOf(grandTotal(mCartProducts)));
         return v;
     }
 
 
-    public int grandTotal(List<myshoes.com.myshoes.model.Cart> items) {
-
-        int totalPrice = 0;
-        for (int i = 0; i < items.size(); i++) {
-            totalPrice += Integer.parseInt(items.get(i).getProductPrice());
-        }
-        total.setText(String.valueOf(totalPrice));
-        return totalPrice;
-    }
+//    public int grandTotal(List<myshoes.com.myshoes.model.Cart> items) {
+//
+//        int totalPrice = 0;
+//        for (int i = 0; i < items.size(); i++) {
+//            totalPrice += Integer.parseInt(items.get(i).getProductPrice());
+//        }
+//        total.setText(String.valueOf(totalPrice));
+//        return totalPrice;
+//    }
 
     private List<myshoes.com.myshoes.model.Cart> retrieveFromDatabase() {
         List<myshoes.com.myshoes.model.Cart> retrievedData = new CartDbHelper(getActivity()).getData();
